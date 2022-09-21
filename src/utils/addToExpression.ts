a6021132dca4d578a83f9dbd6994b6be8fff8b3b
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-21 19:51:38 +0300

    fix addToExpression util

--- src/utils/addToExpression.ts
+++ src/utils/addToExpression.ts
@@ -2,15 +2,17 @@ const operations = ['/', '*', '+', '-'];
 
 export const addToExpression = (expression: string, value: string) => {
   let result = String(expression);
-  console.log(result, typeof result);
   const lastCharIsOperation = operations.includes(result[result.length - 1]);
   const valueIsOperation = operations.includes(value);
   if (lastCharIsOperation && valueIsOperation) {
     return result.slice(0, result.length - 1) + value;
   }
   if (result === '0' && !operations.includes(value)) {
-    console.log(1);
     result = value;
+    return result;
+  }
+  if (result === '-0' && !operations.includes(value)) {
+    result = '-' + value;
     return result;
   }
   return result + value;
