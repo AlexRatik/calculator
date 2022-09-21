bc45dc24017ad8376cad0fcd2a8a2026ae1b0374
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-21 17:15:36 +0300

    create backspaceToExpression util

---
+++ src/utils/backspaceToExpression.ts
@@ -0,0 +1,7 @@
+export const backspaceToExpression = (expression: string) => {
+  if (expression.length === 1) {
+    return '0';
+  } else {
+    return expression.slice(0, expression.length - 1);
+  }
+};
