4bd0d179dae244c53270d98ea86500230df69d7c
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-21 12:24:31 +0300

    fix styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { AppRouter } from './appRouter';
 import { useAppSelector } from '@hooks/hooks';
 import { ThemeProvider } from 'styled-components';
+import { Header } from '@components/header';
+import { GlobalStyles } from '@styles';
 
 export const App = () => {
-  const { theme } = useAppSelector((state) => state.theme);
-
+  const { configuration } = useAppSelector((state) => state.theme);
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={configuration}>
+      <Header />
       <AppRouter />
+      <GlobalStyles />
     </ThemeProvider>
   );
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,7 @@
 const font = 'sans-serif';
 
-const black = '#000';
-const white = '#fff';
+const black = '#000000';
+const white = '#ffffff';
 const error = '#c86464';
 const primaryLight = '#fdd1d1';
 const secondaryLight = white;
@@ -18,8 +18,6 @@ const defaultTheme = {
   font,
   spaces: ['0', '4px', '8px', '16px', '32px', '64px', '128px'],
   fontSizes: ['12px', '14px', '16px', '20px', '24px', '32px', '40px', '56px', '72px', '80px'],
-  fontWeights: [200, 400, 600],
-  borderRadiuses: ['0', '5px', '10px', '20px'],
   keySize: '80px',
   headerHeight: '80px',
   slimBorderWidth,
