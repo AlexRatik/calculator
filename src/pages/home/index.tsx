754250ab04fc6b7b0b7426fb94126f680a981ba2
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-21 17:17:06 +0300

    fix Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import { ErrorBoundary } from '@components/errorBoundary';
+import { HomeContainer, Wrapper } from './components';
+import { Display } from '@components/display';
+import { Keyboard } from '@components/keyboard';
+import { useAppDispatch, useAppSelector } from '@hooks/hooks';
+import {
+  backspaceExpression,
+  clearExpression,
+  clearHistory,
+  equalExpression,
+  setTap,
+} from '@slices/calculatorSlice';
+import { keyboardEnum } from '@constants/keyboard';
+import { History } from '@components/history';
 
 export const Home = () => {
+  const { expression, history, error } = useAppSelector((state) => state.calculator);
+  const dispatch = useAppDispatch();
+
+  const tap = (value: string) => {
+    if (value === keyboardEnum.CLEAR) {
+      dispatch(clearExpression());
+    } else if (value === keyboardEnum.CLEAR_ENTRY) {
+      dispatch(backspaceExpression());
+    } else if (value === keyboardEnum.EQUAL) {
+      dispatch(equalExpression());
+    } else {
+      dispatch(setTap(value));
+    }
+  };
+
   return (
-    <ErrorBoundary errorMessage={'Something went wrong...'}>
-      <p>fsagwf</p>
-    </ErrorBoundary>
+    <HomeContainer>
+      <ErrorBoundary errorMessage={'Something went wrong...'}>
+        <Wrapper>
+          <Display value={expression} error={error} />
+          <Keyboard
+            onClick={(val: string) => {
+              tap(val);
+            }}
+          />
+        </Wrapper>
+        <History data={history} clearHistory={() => dispatch(clearHistory())} />
+      </ErrorBoundary>
+    </HomeContainer>
   );
 };
