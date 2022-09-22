1fc129cc3ce8d123ffaa34a986f9565dd6d20f55
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-22 15:28:50 +0300

    fix addToExpression

--- src/utils/addToExpression.ts
+++ src/utils/addToExpression.ts
@@ -1,17 +1,17 @@
-const operations = ['/', '*', '+', '-'];
+const operationsAndDot = ['.', '/', '*', '+', '-'];
 
 export const addToExpression = (expression: string, value: string) => {
   let result = String(expression);
-  const lastCharIsOperation = operations.includes(result[result.length - 1]);
-  const valueIsOperation = operations.includes(value);
+  const lastCharIsOperation = operationsAndDot.includes(result[result.length - 1]);
+  const valueIsOperation = operationsAndDot.includes(value);
   if (lastCharIsOperation && valueIsOperation) {
     return result.slice(0, result.length - 1) + value;
   }
-  if (result === '0' && !operations.includes(value)) {
+  if (result === '0' && !operationsAndDot.includes(value)) {
     result = value;
     return result;
   }
-  if (result === '-0' && !operations.includes(value)) {
+  if (result === '-0' && !operationsAndDot.includes(value)) {
     result = '-' + value;
     return result;
   }
