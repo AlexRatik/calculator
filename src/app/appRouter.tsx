d754b5eab56bc1ccc85980f09dbfc4e4fc0f3a39
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-22 15:31:38 +0300

    add settings's route

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '@pages/home';
+import { Settings } from '@pages/settings';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/fc" element={<Home />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   );
 };
