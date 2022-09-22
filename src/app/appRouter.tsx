5205efcd705f1b9b4e5dbc638094edd646c16d20
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-22 17:02:31 +0300

    update router

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { Home } from '@pages/home';
 import { Settings } from '@pages/settings';
 import { HomeOnClass } from '@pages/home';
@@ -7,9 +7,13 @@ import { HomeOnClass } from '@pages/home';
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/fc" element={<Home />} />
-      <Route path="/cc" element={<HomeOnClass />} />
-      <Route path="/settings" element={<Settings />} />
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="fc" element={<Home />} />
+        <Route path="cc" element={<HomeOnClass />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
