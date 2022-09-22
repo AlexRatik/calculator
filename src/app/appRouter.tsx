d9b8f05ffac46b19aab8ed45fe0a58325f8ed852
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-22 16:52:30 +0300

    update router

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '@pages/home';
 import { Settings } from '@pages/settings';
+import { HomeOnClass } from '@pages/home';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/fc" element={<Home />} />
+      <Route path="/cc" element={<HomeOnClass />} />
       <Route path="/settings" element={<Settings />} />
     </Routes>
   );
