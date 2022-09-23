f5d1148b202bfa4d8e939f008e2b1455649b9e55
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-23 14:05:29 +0300

    fix transformResult function

--- src/utils/transformResult.ts
+++ src/utils/transformResult.ts
@@ -1,9 +1,4 @@
-export const transformResult = (val: number) => {
-  const str = String(val);
-  const dotIndex = str.indexOf('.');
-  if (dotIndex === -1 || str.length - dotIndex <= 3) {
-    return +val;
-  } else {
-    return (+val).toFixed(3);
-  }
+export const transformResult = (val: string) => {
+  const res = (+val).toFixed(3);
+  return +res;
 };
