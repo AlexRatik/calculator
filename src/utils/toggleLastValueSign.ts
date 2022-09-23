284b09b18411c7248cf1af2e0bd263664909f578
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-23 14:05:18 +0300

    fix toggleSign function

--- src/utils/toggleLastValueSign.ts
+++ src/utils/toggleLastValueSign.ts
@@ -1,27 +1,42 @@
-const operations = ['/', '*', '+', '-'];
+const operations = ['/', '*', '+', '-', '%', '(', ')'];
 
-export const toggleLastValueSign = (expr: string) => {
-  console.log(expr);
+const reverseAndJoin: (val: string) => string = (str: string) => {
+  return str.split('').reverse().join('');
+};
+
+export const toggleLastValueSign: (val: string) => string = (expr: string) => {
   const arr = expr.split('');
   let value = '';
-  let index = 0;
+  let result = '';
+
   for (let i = arr.length - 1; i >= 0; i--) {
     if (!operations.includes(arr[i])) {
       value += arr[i];
     } else {
-      if (arr[i] === '-' && arr[i - 1] == undefined) {
-        return value.split('').reverse().join('');
+      if (arr[i] === ')') {
+        continue;
       }
-      if (arr[i] === '-' && operations.includes(arr[i - 1])) {
-        return expr.slice(0, i) + value.split('').reverse().join('');
+
+      // For example: (-4) => 4
+      if (arr[i] === '-' && arr[i - 1] == '(' && arr[i - 2] === undefined) {
+        result = reverseAndJoin(value);
+        break;
       }
-      if (operations.includes(arr[i]) && +arr[i - 1] > 0) {
-        return expr.slice(0, i + 1) + '-' + value.split('').reverse().join('');
+      // For example: 2+(-6) => 2+6
+      if (arr[i] === '-' && arr[i - 1] === '(') {
+        result = expr.slice(0, i - 1) + reverseAndJoin(value);
+        break;
       }
-      index = i;
-      break;
+      //if arr[i] is operation and arr[i-1] is number. For example: 9+5 => 9+(-5)
+      if (+arr[i - 1] >= 0 && i === arr.length - 1) {
+        result = expr.slice(0, i + 1) + `(-`;
+        break;
+      }
+      if (+arr[i - 1] >= 0) {
+        result = expr.slice(0, i + 1) + `(-${reverseAndJoin(value)})`;
+        break;
+      }
     }
   }
-  value += '-';
-  return expr.slice(0, index) + value.split('').reverse().join('');
+  return result || `(-${reverseAndJoin(value)})`;
 };
