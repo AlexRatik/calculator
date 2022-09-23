1063119c74571810852acbf4cd116a03bbb1956c
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-09-24 02:16:05 +0300

    update display component

--- src/components/display/display.tsx
+++ src/components/display/display.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import { DisplayWrapper, ErrorWrapper } from '@components/display/components';
+import { memo } from 'react';
 
 interface DisplayProps {
   value: string;
   error: string;
 }
 
-export const Display = ({ value, error }: DisplayProps) => {
+export const Display = memo(({ value, error }: DisplayProps) => {
   return (
-    <DisplayWrapper>
-      <p>{value}</p>
-      <ErrorWrapper>{error}</ErrorWrapper>
+    <DisplayWrapper data-testid="display">
+      <p data-testid="display_text">{value}</p>
+      <ErrorWrapper data-testid="display_error">{error}</ErrorWrapper>
     </DisplayWrapper>
   );
-};
+});
--- src/components/display/displayOnClass.tsx
+++ src/components/display/displayOnClass.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { DisplayWrapper, ErrorWrapper } from '@components/display/components';
 
 interface DisplayOnClassProps {
@@ -6,12 +6,13 @@ interface DisplayOnClassProps {
   error: string;
 }
 
-export class DisplayOnClass extends Component<DisplayOnClassProps> {
+export class DisplayOnClass extends PureComponent<DisplayOnClassProps> {
   render() {
+    const { error, value } = this.props;
     return (
-      <DisplayWrapper>
-        <p>{this.props.value}</p>
-        <ErrorWrapper>{this.props.error}</ErrorWrapper>
+      <DisplayWrapper data-testid="display">
+        <p data-testid="display_text">{value}</p>
+        <ErrorWrapper data-testid="display_error">{error}</ErrorWrapper>
       </DisplayWrapper>
     );
   }
